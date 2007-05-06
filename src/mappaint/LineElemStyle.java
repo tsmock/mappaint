8468752f9c80235b922ddb1cd383e05574c1e567
Damian Sulewski <damian.sulewski@uni-dortmund.de>
2007-05-06 16:12:25 +0000

    added useRealWidth function

---
+++ src/mappaint/LineElemStyle.java
@@ -0,0 +1,37 @@
+package mappaint;
+import java.awt.Color;
+
+public class LineElemStyle extends ElemStyle
+{
+    int width;
+    int realWidth = 0; //the real width of this line in meter
+    Color colour;
+
+    public LineElemStyle (int width, int realWidth, Color colour, int minZoom)
+    {
+        this.width = width;
+        this.realWidth = realWidth;
+        this.colour = colour;
+        this.minZoom = minZoom;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public int getRealWidth()
+    {
+        return realWidth;
+    }
+
+    public Color getColour()
+    {
+        return colour;
+    }
+
+    @Override public String toString()
+    {
+        return "LineElemStyle:  width= " + width +  " colour=" + colour;
+    }
+}
