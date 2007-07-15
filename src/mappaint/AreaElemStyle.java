20934af92b5549fc4e6aaca933f812cb94247fe8
Ulf Lamping <ulf.lamping@googlemail.com>
2007-07-15 15:09:07 +0000

    implement a way to set both maxScale and minScale, exchanging former zoom funtionality

---
+++ src/mappaint/AreaElemStyle.java
@@ -0,0 +1,24 @@
+package mappaint;
+import java.awt.Color;
+
+public class AreaElemStyle extends ElemStyle
+{
+    Color colour;
+
+    public AreaElemStyle (Color colour, int maxScale, int minScale)
+    {
+        this.colour = colour;
+        this.maxScale = maxScale;
+        this.minScale = minScale;
+    }
+
+    public Color getColour()
+    {
+        return colour;
+    }
+
+    @Override public String toString()
+    {
+        return "AreaElemStyle:   colour=" + colour;
+    }
+}
---
+++ src/mappaint/ElemStyle.java
@@ -0,0 +1,20 @@
+package mappaint;
+
+abstract public class ElemStyle
+{
+    // zoom range to display the feature
+    protected int minScale;
+    protected int maxScale;
+
+    public int getMinScale()
+    {
+        return minScale;
+    }
+    public int getMaxScale()
+    {
+        return maxScale;
+    }
+}
+
+
+
